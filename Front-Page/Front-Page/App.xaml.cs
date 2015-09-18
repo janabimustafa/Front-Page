75aad020567937dbbf02b776fadea093dda080ae
Mustafa Taleb <mustafa@mustafataleb.com>
2015-09-18 08:33:17 -0500

    Implemented Template10 (basic)

--- Front-Page/Front-Page/App.xaml.cs
+++ Front-Page/Front-Page/App.xaml.cs
@@ -1,108 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Front_Page.Services.SettingsServices;
+using Template10.Common;
 
 namespace Front_Page
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    sealed partial class App : BootStrapper
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            InitializeComponent();
+
+            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-Cache
+            CacheMaxDuration = TimeSpan.FromDays(2);
+
+            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-BackButton
+            ShowShellBackButton = SettingsService.Instance.UseShellBackButton;
+
+            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SplashScreen
+            SplashFactory = (e) => new Views.Splash(e);
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        // runs even if restored from state
+        public override Task OnInitializeAsync(IActivatedEventArgs args)
         {
+            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SplitView
+            var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
+            Window.Current.Content = new Views.Shell(nav);
 
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
-
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
-            {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
-            }
-
-            if (rootFrame.Content == null)
-            {
-                // When the navigation stack isn't restored navigate to the first page,
-                // configuring the new page by passing required information as a navigation
-                // parameter
-                rootFrame.Navigate(typeof(MainPage), e.Arguments);
-            }
-            // Ensure the current window is active
-            Window.Current.Activate();
+            return Task.FromResult<object>(null);
         }
 
-        /// <summary>
-        /// Invoked when Navigation to a certain page fails
-        /// </summary>
-        /// <param name="sender">The Frame which failed navigation</param>
-        /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        // runs only when not restored from state
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
-            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+            await Task.Delay(0);
+            NavigationService.Navigate(typeof(Views.MainPage));               
+            // start user experience
+            //switch (DetermineStartCause(args))
+            //{
+            //    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-Toast
+            //    //case AdditionalKinds.Toast:
+
+            //    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SecondaryTile
+            //    //case AdditionalKinds.SecondaryTile:
+            //    //    var e = (args as ILaunchActivatedEventArgs);
+            //    //    NavigationService.Navigate(typeof (Views.DetailPage), e.Arguments);
+            //    //    break;
+
+            //    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-NavigationService
+            //    default:
+            //        NavigationService.Navigate(typeof(Views.MainPage));
+            //        break;
+            //}            
         }
 
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="sender">The source of the suspend request.</param>
-        /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
-            deferral.Complete();
-        }
+
     }
 }
---
+++ Front-Page/Front-Page/Views/Shell.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Template10.Services.NavigationService;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Front_Page.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Shell : Page
+    {
+
+        private static Shell Instance { get; set; }
+
+        public Shell(NavigationService navigationService)
+        {
+            Instance = this;
+            this.InitializeComponent();
+            MyHamburgerMenu.NavigationService = navigationService;
+        }
+
+        public static void SetBusyIndicator(bool busy, string text = null)
+        {
+            Instance.BusyIndicator.Visibility = (busy)
+                ? Visibility.Visible : Visibility.Collapsed;
+            Instance.BusyRing.IsActive = busy;
+            Instance.BusyText.Text = text ?? string.Empty;
+        }
+    }
+}
