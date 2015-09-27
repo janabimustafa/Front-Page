54db163ef3293a72995479f19bca0985aed6df34
Mustafa Taleb <mustafa@mustafataleb.com>
2015-09-27 06:29:05 -0500

    Implemented Travis CI

--- Front-Page/Front-Page/App.xaml.cs
+++ Front-Page/Front-Page/App.xaml.cs
@@ -1,65 +1,22 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Windows.ApplicationModel.Activation;
-using Windows.UI.Xaml;
-using Front_Page.Services.SettingsServices;
-using Template10.Common;
 
 namespace Front_Page
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    sealed partial class App : BootStrapper
+    /// Documentation on APIs used in this page:
+    /// https://github.com/Windows-XAML/Template10/wiki
+
+    sealed partial class App : Template10.Common.BootStrapper
     {
         public App()
         {
             InitializeComponent();
-
-            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-Cache
-            CacheMaxDuration = TimeSpan.FromDays(2);
-
-            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-BackButton
-            ShowShellBackButton = SettingsService.Instance.UseShellBackButton;
-
-            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SplashScreen
-            SplashFactory = (e) => new Views.Splash(e);
         }
 
-        // runs even if restored from state
-        public override Task OnInitializeAsync(IActivatedEventArgs args)
+        public override Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
         {
-            // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SplitView
-            var nav = NavigationServiceFactory(BackButton.Attach, ExistingContent.Include);
-            Window.Current.Content = new Views.Shell(nav);
-
+            NavigationService.Navigate(typeof(Views.MainPage));
             return Task.FromResult<object>(null);
         }
-
-        // runs only when not restored from state
-        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
-        {
-            await Task.Delay(0);
-            NavigationService.Navigate(typeof(Views.MainPage));               
-            // start user experience
-            //switch (DetermineStartCause(args))
-            //{
-            //    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-Toast
-            //    //case AdditionalKinds.Toast:
-
-            //    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-SecondaryTile
-            //    //case AdditionalKinds.SecondaryTile:
-            //    //    var e = (args as ILaunchActivatedEventArgs);
-            //    //    NavigationService.Navigate(typeof (Views.DetailPage), e.Arguments);
-            //    //    break;
-
-            //    // DOCS: https://github.com/Windows-XAML/Template10/wiki/Docs-%7C-NavigationService
-            //    default:
-            //        NavigationService.Navigate(typeof(Views.MainPage));
-            //        break;
-            //}            
-        }
-
-
     }
 }
--- Front-Page/Front-Page/Properties/AssemblyInfo.cs
+++ Front-Page/Front-Page/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Front-Page")]
+[assembly: AssemblyTitle("Front_Page")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Front-Page")]
+[assembly: AssemblyProduct("Front_Page")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
